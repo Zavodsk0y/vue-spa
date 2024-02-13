efff917cbe2809ecc4fc01590ee6d13182b6a726
Zavodsk0y <zimniyvolk5@gmail.com>
2024-02-13 14:25:25 +0700

    1

---
+++ my-app/src/components/Products.vue
@@ -0,0 +1,35 @@
+<script>
+import store from "@/store";
+import AUTH_REQUEST from "@/store"
+export default {
+  name: "Login",
+  data() {
+    return {
+      products: []
+    }
+  },
+  methods: {
+  },
+  mounted() {
+    this.$store
+        .dispatch('GET_PRODUCTS_REQUEST')
+    this.products = this.$store.getters.SET_PRODUCTS
+  }
+}
+</script>
+
+<template>
+  <section>
+    <article>
+      <div v-for="product in products" :key="product.id">
+        <span>Name: {{product.name}}</span>
+        <span>Description: {{product.description}}</span>
+      </div>
+    </article>
+  </section>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ my-app/src/views/ProductsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="products">
+    <Products></Products>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {defineComponent} from "vue";
+import Products from "@/components/Products.vue";
+
+export default defineComponent({
+  components: {Products}
+})
+</script>
